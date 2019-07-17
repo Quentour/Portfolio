54232cdfa546e0324a430d2c53bf076750b522b4
Quentin LATOUR <Quentour69100@gmail.com>
2019-07-17 10:50:32 +0200

    started working on the project detail view

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ Encore
     .addEntry('app', './assets/js/app.js')
     .addEntry('home', './assets/js/home.js')
     .addEntry('index', './assets/js/index.js')
+    .addEntry('show', './assets/js/show.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
